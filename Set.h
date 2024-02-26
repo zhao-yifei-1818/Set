37163518c206ee4915bc4fd7b5a566d029286627
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-02-26 02:23:33 +0000

    Feb 25, 2024, 6:23 PM

--- Set.h
+++ Set.h
@@ -1,14 +1,17 @@
 
 #ifndef SET_H
 #define SET_H
-
+template<typename T>
 class Set {
 private:
   T* item;
+  int currentSize;
+  int capacity;
 
 public:
-  Set();
+  Set(); // initial capacity of 8
   ~Set();
+  Set(const Set& other); // copy constructor
   void add(T item);
   int getSize() const;
   void remove(T item);
@@ -18,5 +21,30 @@ public:
   string toString() const;
   Set intersectionWith(const Set& other) const;
 };
+
+template<typename T>
+Set<T>::Set()
+{
+  currentSize = 0;
+  capacity = 8;
+  item = new T[capacity];
+} // no arg constructor
+
+template<typename T>
+Set<T>::~Set()
+{
+  delete[] item;
+} // Destructor
+
+template<typename T>
+Set<T>::Set(const Set<T>& other)
+{
+  currentSize = other.currentSize;
+  capacity = other.capacity;
+
+  item = new T[capacity];
+  for (int i = 0; i < capacity; i++)
+    item[i] = other.item[i];
+} // Copy constructor
 
 #endif // SET_H
