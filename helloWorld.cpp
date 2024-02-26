0e8a218332574b5a75944025661e230b5a36807b
zhao-yifei-1818 <williamfei0000@gmail.com>
2024-02-26 03:04:17 +0000

    Feb 25, 2024, 7:04 PM

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,11 +1,13 @@
-using namespace std;
 #include <iostream>
 #include <string>
+
+using namespace std;
 
 int main()
 {
   string returnstring;
-  cin >> "enter your sentence:" >> returnstring;
-  cout << "Hello World" << endl;
+  cout << "Enter your sentence: ";
+  cin >> returnstring;
+  cout << "Hello, World!" << endl;
   return 0;
 }
